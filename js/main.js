e2e73f15d20960294edb64ae4238cf60d1f04451
Tomas Lozano <lozanotdlc@gmail.com>
2024-11-14 23:04:31 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const typed = new Typed(".auto-type", {
+    strings: ["Contenedores.", "Oficinas.", "Módulos Habitacionales.", "Pañoles.", "Atención al Cliente.", "Presupuestos."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+})
