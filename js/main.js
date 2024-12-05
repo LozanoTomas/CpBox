1f25c8c984aa3b31e17d37593950d21222b8d3c7
Tomas Lozano <lozanotdlc@gmail.com>
2024-12-05 11:39:16 -0300

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".auto-type", {
-    strings: ["Contenedores.", "Oficinas.", "Módulos Habitacionales.", "Pañoles.", "Atención al Cliente.", "Presupuestos."],
+    strings: ["Obradores.", "Oficinas.", "Módulos Habitacionales.", "Pañoles.", "Atención al Cliente.", "Presupuestos."],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true,
