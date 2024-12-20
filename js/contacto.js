ac66c9c35e9c9e97b67290b4f163a125fe665c06
Tomas Lozano <lozanotdlc@gmail.com>
2024-12-20 13:27:33 -0300

    ultimos cambios

--- js/contacto.js
+++ js/contacto.js
@@ -6,7 +6,7 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event){
     event.preventDefault()
     const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:lozanotdlc@gmail.com?subject=Nombre: ${form.get('name')} Apellido: ${form.get('subname')} Correo: ${form.get('email')}&body=Mensaje: ${form.get('message')}`)
+    $buttonMailto.setAttribute('href', `mailto:roxyrivas2003@yahoo.com.ar?subject=Nombre: ${form.get('name')} Apellido: ${form.get('subname')} Correo: ${form.get('email')}&body=Mensaje: ${form.get('message')}`)
     $buttonMailto.click()
 }
 
